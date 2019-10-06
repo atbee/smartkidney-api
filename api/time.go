86f1b671ac03e6e96bd0870b9f45e9bd6251fc58
atb <p_athibet@odds.team>
2019-10-07 02:16:37 +0700

    create startTime and endTime func in UTC+07

--- api/time.go
+++ api/time.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"log"
 	"net/http"
 	"time"
 
@@ -12,4 +13,31 @@ func (db *MongoDB) GetTime(c echo.Context) (err error) {
 	t := time.Now()
 
 	return c.JSON(http.StatusOK, t)
+}
+
+// StartDate create a time to start searching in UTC +07.
+func StartDate(s string) time.Time {
+	const shortForm = "2006-01-02"
+	loc, err := time.LoadLocation("Asia/Bangkok")
+	if err != nil {
+		log.Println("cannot load location Asia/Bangkok.")
+	}
+
+	t, err := time.Parse(shortForm, s)
+	if err != nil {
+		log.Println("cannot convert the date")
+	}
+
+	t = t.In(loc)
+	t = t.Add(-7 * time.Hour)
+
+	return t
+}
+
+// EndDate create a time to end the search.
+func EndDate(s string) time.Time {
+	t := StartDate(s)
+	t = t.AddDate(0, 0, 1)
+
+	return t
 }
